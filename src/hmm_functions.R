5c4b70f634a0a31e31b2a7ff4fb812995de6e97e
Adrian Torchiana <adrian.torchiana@gmail.com>
2019-09-30 19:29:06 -0700

    Add time homogeneous estimation functions (for validation exercise)

--- src/hmm_functions.R
+++ src/hmm_functions.R
@@ -579,3 +579,295 @@ get_hmm_panel_from_points <- function(points_dt, discrete_y_varname, max_panel_s
     })
     return(panel)
 }
+
+baum_welch_time_homogeneous <- function(panel_element, params) {
+    ## Baum-Welch algorithm for HMM with discrete hidden x, discrete observations y (NAs allowed)
+    ## Written following Ramon van Handel's HMM notes, page 40, algorithm 3.2
+    ## https://www.princeton.edu/~rvan/orf557/hmm080728.pdf
+    ## Careful, his observation index is in {0, 1, ... , n} while I use {1, 2, ... , y_length}
+    stopifnot(valid_panel_element(panel_element, params))
+    stopifnot(valid_parameters_time_homogeneous(params))
+    y_length <- length(panel_element$y)
+    stopifnot(y_length > 1)
+    c <- vector("numeric", y_length)
+    if(is.na(panel_element$y[1])) {
+        upsilon <- rep(1, params$n_components)
+    } else {
+        upsilon <- params$pr_y[, panel_element$y[1]]  # Time-invariant pr_y
+    }
+    stopifnot(length(upsilon) == params$n_components)
+    mu <- params$mu
+    c[1] <- sum(upsilon * mu)
+    ## Matrix pi_contemporaneous gives probabilities over x_k conditional on {y_1, y_2, ... , y_k}
+    ## Notation in van Handel's HMM notes is pi_k, whereas pi_{k|n} conditions on full history of y
+    pi_contemporaneous <- matrix(NA, params$n_components, y_length)
+    pi_contemporaneous[, 1] <- upsilon * mu / c[1]
+    upsilon_list <- list()
+    upsilon_list[[1]] <- upsilon
+    for(k in seq(2, y_length)) {
+        ## Forward loop
+        if(is.na(panel_element$y[k])) {
+            upsilon <- rep(1, params$n_components)
+        } else {
+            upsilon <- params$pr_y[, panel_element$y[k]]  # Time-invariant pr_y
+        }
+        upsilon_list[[k]] <- upsilon  # Cache for backward loop
+        pi_tilde <- upsilon * t(params$P) %*% pi_contemporaneous[, k-1]
+        c[k] <- sum(pi_tilde)
+        pi_contemporaneous[, k] <- pi_tilde  / c[k]
+    }
+    beta <- matrix(NA, params$n_components, y_length)
+    beta[, y_length] <- 1 / c[y_length]
+    ## Matrix pi gives probabilities over x conditional on full history of y
+    ## Notation in van Handel's HMM notes is pi_{k|n}, as opposed to pi_k
+    pi <- matrix(NA, params$n_components, y_length)
+    pi[, y_length] <- pi_contemporaneous[, y_length]
+    pi_transition_list <- list()  # List of posterior probabilities over hidden x transitions
+    for(k in seq(1, y_length - 1)) {
+        ## Backward loop
+        upsilon <- diag(upsilon_list[[y_length - k + 1]], params$n_components, params$n_components)
+        pi_matrix <- diag(pi_contemporaneous[, y_length - k],
+                          params$n_components, params$n_components)
+        beta_matrix <- diag(beta[, y_length - k + 1], params$n_components, params$n_components)
+        beta[, y_length - k] <- params$P %*% upsilon %*% beta[, y_length - k + 1] / c[y_length - k]
+        pi_transition_list[[y_length - k]] <- pi_matrix %*% params$P %*% upsilon %*% beta_matrix
+        stopifnot(isTRUE(all.equal(sum(pi_transition_list[[y_length - k]]), 1.0)))
+        pi[, y_length - k] <- rowSums(pi_transition_list[[y_length - k]])
+    }
+    loglik <- sum(log(c))
+    return(list(loglik=loglik, pi=pi, pi_transition_list=pi_transition_list))
+}
+
+em_parameter_estimates_time_homogeneous <- function(panel, params, max_iter, epsilon=0.001) {
+    ## EM for panel of independent HMM realizations; stop at max_iter or distance < epsilon
+    ## Written following Ramon van Handel's HMM notes page 87, algorithm 6.1, modified for panel data
+    ## https://www.princeton.edu/~rvan/orf557/hmm080728.pdf
+    message("starting em ", Sys.time())
+    stopifnot(valid_parameters_time_homogeneous(params))
+    observation_lengths <- vapply(panel, function(panel_element) {
+        length(panel_element$y)
+    }, FUN.VALUE=1)  # Observation lengths should be the same in each panel element
+    stopifnot(all(observation_lengths > 1) && length(unique(observation_lengths)) == 1)
+    observation_length <- observation_lengths[1]
+    iteration <- 1
+    while(iteration <= max_iter) {
+        baum_welch_list <- lapply(panel, baum_welch_time_homogeneous, params=params)
+        panel_with_baum_welch <- Map(c, panel, baum_welch_list)
+        ## Update transition probabilities
+        numerators <- unlist(lapply(baum_welch_list, function(baum_welch_element) {
+            return(baum_welch_element$pi_transition_list)
+        }), recursive=FALSE)
+        stopifnot(is.list(numerators) && is.matrix(numerators[[1]]))
+        denominators <- unlist(lapply(baum_welch_list, function(baum_welch_element) {
+            lapply(seq_len(ncol(baum_welch_element$pi) - 1), function(time) {
+                matrix(baum_welch_element$pi[, time], params$n_components, params$n_components)
+            })
+        }), recursive=FALSE)
+        stopifnot(is.list(denominators) && is.matrix(denominators[[1]]))
+        stopifnot(length(numerators) == length(denominators))
+        numerator <- Reduce("+", numerators)
+        denominator <- Reduce("+", denominators)
+        stopifnot(all(rowSums(denominator) > 0))  # Can fail if a state has zero probability
+        stopifnot(all(denominator > 0))
+        updated_P <- numerator / denominator
+        stopifnot(isTRUE(all.equal(rowSums(updated_P), rep(1, params$n_components))))
+        ## Update initial probabilities over hidden state
+        mus <- lapply(baum_welch_list, function(baum_welch_element) {
+            baum_welch_element$pi[, 1]  # Initial distribution
+        })
+        updated_mu <- Reduce("+", mus) / length(panel)
+        stopifnot(isTRUE(all.equal(sum(updated_mu), 1)))
+        ## Update time-invariant observation probabilities; careful, observation vector can contain NAs
+        pr_y_weights <- lapply(panel_with_baum_welch, function(panel_element, params) {
+            y_non_NA <- matrix(1 * !is.na(panel_element$y),
+                               nrow(params$pr_y), length(panel_element$y), byrow=TRUE)
+            rowSums(panel_element$pi * y_non_NA)  # Sum over time
+        }, params)
+        pr_y_matrices <- lapply(panel_with_baum_welch, function(panel_element, params) {
+            column_list <- lapply(seq_len(ncol(params$pr_y)), function(curr_y) {
+                    y_indicators <- matrix(!is.na(panel_element$y) &
+                                           panel_element$y == curr_y,
+                                           nrow(params$pr_y),
+                                           length(panel_element$y), byrow=TRUE)
+                    return(rowSums(panel_element$pi * y_indicators))  # Sum over time
+            })
+            return(do.call(cbind, column_list))
+        }, params)
+        updated_pr_y <- (Reduce("+", pr_y_matrices) / Reduce("+", pr_y_weights))
+        stopifnot(isTRUE(all.equal(rowSums(updated_pr_y), rep(1, nrow(updated_pr_y)))))
+        ## Take sup norm between previous parameters and updated values
+        P_distance <- max(abs(params$P - updated_P))
+        mu_distance <- max(abs(params$mu - updated_mu))
+        pr_y_distance <- max(abs(params$pr_y - updated_pr_y))
+        distance <- max(mu_distance, P_distance, pr_y_distance)
+        loglik <- sum(vapply(baum_welch_list, function(x) x$loglik, FUN.VALUE=1))
+        message("iteration ", iteration, " distance ", round(distance, 4), " loglik ", round(loglik, 4))
+        if("distance" %in% names(params)) {
+            params$distance <- c(params$distance, distance)
+        } else {
+            params$distance <- distance  # Maximum distance over all parameters
+        }
+        if("mu_distance" %in% names(params)) {
+            params$mu_distance <- c(params$mu_distance, mu_distance)
+        } else {
+            params$mu_distance <- mu_distance  # Distance for initial distribution over x
+        }
+        if("pr_y_distance" %in% names(params)) {
+            params$pr_y_distance <- c(params$pr_y_distance, pr_y_distance)
+        } else {
+            params$pr_y_distance <- pr_y_distance
+        }
+        if("P_distance" %in% names(params)) {
+            params$P_distance <- c(params$P_distance, max(P_distance))
+        } else {
+            params$P_distance <- max(P_distance)
+        }
+        if("n_em_iterations" %in% names(params)) {
+            params$n_em_iterations <- params$n_em_iterations + 1  # Could be useful to track
+        } else {
+            params$n_em_iterations <- 1
+        }
+        params$mu <- updated_mu
+        params$P <- updated_P
+        params$pr_y <- updated_pr_y
+        if("loglik" %in% names(params)) {
+            params$loglik <- c(params$loglik, loglik)  # Save history of log likelihoods
+        } else {
+            params$loglik <- loglik
+        }
+        params$panel_size <- length(panel)
+        if(distance < epsilon) break
+        iteration <- iteration + 1
+        rm(panel_with_baum_welch)
+        rm(baum_welch_list)
+        gc()
+    }
+    message("done running em ", Sys.time())
+    params$time_finished_em <- Sys.time()
+    return(params)
+}
+
+objfn_min_dist_time_homogeneous <- function(x, M_Y_joint_hat_inverse_list, M_Y_joint_hat_list, M_fixed_y_Y_joint_hat_list,
+                                            n_components, W_matrix=NULL, fixed_y=1) {
+    ## Objective function for minimum distance estimation
+    stopifnot(is.vector(x))
+    stopifnot(length(x) == 2*n_components^2 + n_components)  # Misclassification probabilities, transitions, and initial distribution
+    objects <- get_M_Y_given_S_mu_P_from_vector(x, n_components)
+    candidate_M_Y_given_S <- objects$candidate_M_Y_given_S
+    mu <- objects$mu
+    P <- objects$P
+    candidate_M_S_joint_list <- list()
+    for(time_index in seq_len(length(unique(dtable$year)) - 1)) {
+        if(time_index == 1) {
+            mu_t <- mu
+        } else {
+            mu_t <- rowSums(candidate_M_S_joint_list[[time_index - 1]])
+        }
+        candidate_M_S_joint_list[[time_index]] <- t(P * matrix(mu_t, length(mu_t), length(mu_t)))
+    }
+    stopifnot(length(candidate_M_S_joint_list) == length(M_Y_joint_hat_inverse_list))
+    candidate_D_list <- lapply(seq_len(length(unique(dtable$year)) - 2), function(fixed_t) {
+        candidate_M_S_joint <- candidate_M_S_joint_list[[fixed_t + 1]]
+        candidate_P <- candidate_M_S_joint / matrix(colSums(candidate_M_S_joint),
+                                                    nrow(candidate_M_S_joint),
+                                                    ncol(candidate_M_S_joint), byrow=TRUE)  # From t+1 to t+2
+        candidate_D <- matrix(0, n_components, n_components)
+        diag(candidate_D) <- candidate_P %*% candidate_M_Y_given_S[fixed_y, ]
+        return(candidate_D)
+    })
+    stopifnot(length(candidate_D_list) == length(M_fixed_y_Y_joint_hat_list[[1]]))  # Careful with fixed_y
+    g1_vector <- sapply(seq_along(candidate_D_list), function(time_index) {
+        return(norm(M_fixed_y_Y_joint_hat_list[[fixed_y]][[time_index]] %*% M_Y_joint_hat_inverse_list[[time_index]] %*%
+                    candidate_M_Y_given_S -
+                    candidate_M_Y_given_S %*% candidate_D_list[[time_index]], type="F"))
+    })
+    g2_vector <- sapply(seq_along(candidate_M_S_joint_list), function(time_index) {
+        return(norm(candidate_M_Y_given_S %*% candidate_M_S_joint_list[[time_index]] %*% t(candidate_M_Y_given_S) -
+                    M_Y_joint_hat_list[[time_index]], type="F"))
+    })
+    g <- c(g1_vector, g2_vector)
+    if(is.null(W_matrix)) {
+        W_matrix <- diag(length(g))
+    }
+    stopifnot(nrow(W_matrix) == length(g) && ncol(W_matrix) == length(g))
+    return(as.vector(t(g) %*% W_matrix %*% g))
+}
+
+eq_function_min_dist_time_homogeneous <- function(x, M_Y_joint_hat_inverse_list, M_Y_joint_hat_list, M_fixed_y_Y_joint_hat_list,
+                                                  n_components, W_matrix=NULL) {
+    ## Constraint function for minimum distance estimation (constraint is eq_function(x) = 1 everywhere)
+    ## "The main and constraint functions must take the exact same arguments, irrespective of whether they are used"
+    objects <- get_M_Y_given_S_mu_P_from_vector(x, n_components)
+    candidate_M_Y_given_S <- objects$candidate_M_Y_given_S
+    mu <- objects$mu
+    P <- objects$P
+    return(c(colSums(candidate_M_Y_given_S), sum(mu), rowSums(P)))
+}
+
+valid_parameters_time_homogeneous <- function(params) {
+    stopifnot(is.list(params))
+    stopifnot("n_components" %in% names(params))
+    stopifnot("mu" %in% names(params))  # Vector of probabilities for intial distribution
+    stopifnot(length(params$mu) == params$n_components)
+    stopifnot(all(params$mu >= 0))
+    stopifnot(isTRUE(all.equal(sum(params$mu), 1.0)))  # Careful with float comparisons
+    stopifnot("P" %in% names(params))  # Transition matrix for hidden state
+    stopifnot("pr_y" %in% names(params))  # Observation probabilities conditional on x
+    stopifnot(nrow(params$pr_y) == params$n_components)
+    stopifnot(isTRUE(all.equal(rowSums(params$pr_y), rep(1, nrow(params$pr_y)))))  # Float comparison
+    return(TRUE)
+}
+
+viterbi_path_time_homogeneous <- function(panel_element, params) {
+    ## Viterbi algorithm for HMM with discrete hidden x (returns highest probability path for x)
+    ## Written following Ramon van Handel's HMM notes, page 46, algorithm 3.4
+    ## https://www.princeton.edu/~rvan/orf557/hmm080728.pdf
+    ## Careful, his observation index is in {0, 1, ... , n} while I use {1, 2, ... , t_max}
+    stopifnot(valid_panel_element(panel_element, params))
+    stopifnot(valid_parameters_time_homogeneous(params))
+    mu <- params$mu
+    stopifnot(length(mu) == params$n_components)
+    if(is.na(panel_element$y[1])) {
+        upsilon <- rep(1, params$n_components)
+    } else {
+        if("pr_y" %in% names(params)) {
+            upsilon <- params$pr_y[, panel_element$y[1]]
+        } else {
+            upsilon <- params$pr_y_list[[1]][, panel_element$y[1]]
+        }
+    }
+    stopifnot(length(mu) == length(upsilon))  # Same length as state space
+    t_max <- length(panel_element$y)
+    stopifnot(t_max >= 2)
+    v <- matrix(NA, t_max, params$n_components)  # Maximized log likelihoods
+    v[1, ] <- log(mu) + log(upsilon)
+    b <- matrix(NA, t_max, params$n_components)
+    for(k in seq(2, t_max)) {
+        P <- params$P
+        if(is.na(panel_element$y[k])) {
+            upsilon <- rep(1, params$n_components)
+        } else {
+            if("pr_y" %in% names(params)) {
+                upsilon <- params$pr_y[, panel_element$y[k]]
+            } else {
+                upsilon <- params$pr_y_list[[k]][, panel_element$y[k]]
+            }
+        }
+        stopifnot(length(upsilon) == ncol(v))
+        for(i in seq_len(params$n_components)) {
+            b[k, i] <- which.max(v[k-1, ] + log(P[, i]))
+            v[k, i] <- v[k-1, b[k, i]] + log(P[b[k, i], i]) + log(upsilon[i])
+        }
+    }
+    stopifnot(all(!is.na(v)))  # Entire v matrix should be populated
+    most_likely_path <- rep(NA, t_max)
+    most_likely_path[t_max] <- which.max(v[t_max, ])
+    for(k in seq(1, t_max - 1)) {
+        most_likely_path[t_max - k] <- b[t_max - k + 1, most_likely_path[t_max - k + 1]]
+    }
+    stopifnot(is.vector(most_likely_path))
+    stopifnot(length(most_likely_path) == t_max)
+    stopifnot(all(!is.na(most_likely_path)))
+    stopifnot(all(most_likely_path %in% seq_len(params$n_components)))
+    return(most_likely_path)
+}
