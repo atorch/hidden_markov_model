70a8c82bbe21b7f1b39abc31c23bd1f8702d7710
Adrian Torchiana <adrian.torchiana@gmail.com>
2019-10-17 19:24:07 -0700

    In explore_min_dist_output.R, focus on min dist estimates of deforestation rate after adding diag dominant constraint for pr_y

--- src/explore_min_dist_output.R
+++ src/explore_min_dist_output.R
@@ -1,29 +1,42 @@
+source("hmm_functions.R")
+
 max_abs <- function(x) {
     return(max(abs(x)))
 }
 
-## Note: this file was generated _before_ adding diag dominant pr_y logic to hmm_functions.R
+## Note: this file was generated _before_ adding diag dominant pr_y constraint to hmm_functions.R
 infile <- "county_simulation_2019-10-15 20:10:08_iter_3.rds"
 
-## Note: this file was generated _after_ adding diag dominant pr_y logic to hmm_functions.R
-infile <- "county_simulation_2019-10-16 18:51:25_iter_2.rds"
+## Generated _after_ adding diag dominant pr_y constraint to hmm_functions.R
+infile <- "county_simulation_2019-10-17 18:47:20_iter_1.rds"
 
 simulations <- readRDS(infile)
 
 ## Difference between min dist estimates of P and true P
-P_errors <- lapply(simulations, function(x) {
-    c(x$estimates$min_dist_params_hat_best_objfn$P_list, recursive=TRUE) - c(x$params$P_list, recursive=TRUE)
+P_errors_min_dist <- lapply(simulations, function(x) {
+    sapply(x$estimates$min_dist_params_hat_best_objfn$P_list, get_deforestation_prob_from_P) - sapply(x$params$P_list, get_deforestation_prob_from_P)
 })
 
-hist(sapply(P_errors, mean))  # Looks good, means are close to zero
-hist(sapply(P_errors, sd))  # A few std devs are worryingly large
-hist(sapply(P_errors, max_abs))  # Some large errors in min dist estimates of P
+hist(sapply(P_errors_min_dist, mean))  # Biased upwards -- makes sense if estimates hit edge of parameter space with some non-negligible probability
+mean(c(P_errors_min_dist, recursive=TRUE))  # 0.036
+sd(c(P_errors_min_dist, recursive=TRUE))  # 0.11
+hist(sapply(P_errors_min_dist, max_abs))  # Some large errors in min dist estimates of P
 
-max_abs_errors <- sapply(P_errors, max_abs)
+P_errors_em <- lapply(simulations, function(x) {
+    sapply(x$estimates$em_params_hat_best_likelihood$P_list, get_deforestation_prob_from_P) - sapply(x$params$P_list, get_deforestation_prob_from_P)
+})
+
+hist(sapply(P_errors_em, mean))  # Also biased upwards
+mean(c(P_errors_em, recursive=TRUE))  # 0.02, smaller than min dist
+sd(c(P_errors_em, recursive=TRUE))  # 0.07
+hist(sapply(P_errors_em, max_abs))  # Some large errors in em estimates of P, but better than min dist
+
+## For each simulation, what was the largest error in min dist estimates of the deforestation rate?
+max_abs_errors <- sapply(P_errors_min_dist, max_abs)
 
 ## Note: this fraction is much larger with n_points_per_county=100 than with n_points_per_county=500
-mean(max_abs_errors > 0.5)
-indices_large_errors <- which(max_abs_errors > 0.5)
+mean(max_abs_errors > 0.2)
+indices_large_errors <- which(max_abs_errors > 0.2)
 
 simulations_large_errors <- simulations[indices_large_errors]
 
@@ -31,16 +44,13 @@ simulations_large_errors[[1]]$params
 
 simulations_large_errors[[1]]$estimates$min_dist_params_hat_best_objfn
 
-## Something is wrong: this isn't a valid pr_y matrix (it's not diagonally dominant)
 ## Compare to simulations_large_errors[[1]]$params$pr_y
+## Note: the min dist estimate of pr_y can hit the diag dominant constraint (more likely at small sample sizes?)
 simulations_large_errors[[1]]$estimates$min_dist_params_hat_best_objfn$pr_y
 
 ## Compare to simulations_large_errors[[1]]$params$mu
 simulations_large_errors[[1]]$estimates$min_dist_params_hat_best_objfn$mu
 
-## EM did fine in this example
+## EM did fine (or at least better than min dist) in this example
 simulations_large_errors[[1]]$estimates$em_params_hat_best_likelihood$pr_y
 simulations_large_errors[[1]]$estimates$em_params_hat_best_likelihood$P_list
-
-## Note: this is NULL when all min dist estimates had pr_y that are not diagonally dominant
-simulations_large_errors[[1]]$estimates$min_dist_params_hat_diag_dominant_best_objfn
