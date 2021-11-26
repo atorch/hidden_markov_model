fa505bc38c8d7e6c79686d107c9cd4a58e84df62
ted <rosenbat@gmail.com>
2021-11-26 14:16:31 -0500

    Running with 1000x1000 tiles

--- run_mapbiomas.sh
+++ run_mapbiomas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-parallel --jobs 8  "Rscript explore_mapbiomas.R --row {1} --col {2} --grassland_as_forest --combine_other_non_forest --subsample 0.04 --skip_ml_if_md_is_diag_dominant > log_{1}_{2}.txt" ::: {70000..80000..500} ::: {1000..8000..500} 
+parallel --jobs 8  "Rscript explore_mapbiomas.R --row {1} --col {2} --grassland_as_forest --combine_other_non_forest --subsample 0.04 --skip_ml_if_md_is_diag_dominant --n_random_starts_md 1 > log_{1}_{2}.txt" ::: {1..50001..1000} ::: {1..80000..1000} 
 
