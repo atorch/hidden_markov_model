92d366606103ea2712d15573990d4607d9287edd
Adrian Torchiana <adrian.torchiana@gmail.com>
2022-11-11 14:52:47 -0800

    Install specific versions of R packages in Dockerfile

---
+++ install_packages.R
@@ -0,0 +1,14 @@
+library(remotes)
+
+## We use remotes::install_version to install specific versions of several R packages
+install_version("data.table", version="1.14.2")
+install_version("ggplot2", version="3.3.6")
+install_version("ngspatial", version="1.2-2")
+install_version("raster", version="3.5-21")
+install_version("rgdal", version="1.5-32")
+install_version("rgeos", version="0.5-9")
+install_version("Rsolnp", version="1.16")
+install_version("stargazer", version="5.2.3")
+install_version("optparse", version="1.7.3")
+install_version("plot.matrix", version="1.6.2")
+## TODO Also need stringr?
